2f517a876cb6568af2bddc095e53e220957efdd7
Marcio Shikasho <110318997+MarcioShikasho@users.noreply.github.com>
2025-06-22 21:56:13 -0300

    Update views.py

--- PA3/views.py
+++ PA3/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import Group
-
-# Criação do grupo de "Funcionários"
-group, created = Group.objects.get_or_create(name='Gerenciadores')
 
 #a página só é acessada/carregada se o usuário estiver logado
 @login_required 
